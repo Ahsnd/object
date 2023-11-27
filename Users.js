edbefb2c0929ff746818b01d9bb20475ac0b5ddd
Suizo, Rowena G <143061861+Ahsnd@users.noreply.github.com>
2023-11-27 20:21:13 +0800

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,243 @@
+export const Users = [
+  {
+    id: 1,
+    name: {
+      firstname: "Adam",
+      lastname: "Johnson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "Emma",
+      lastname: "Williams",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "Caleb",
+      lastname: "Smith",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "Sophie",
+      lastname: "Brown",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "Daniel",
+      lastname: "Davis",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "Mia",
+      lastname: "Wilson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "Henry",
+      lastname: "Miller",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 8,
+    name: {
+      firstname: "Oliver",
+      lastname: "Taylor",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 9,
+    name: {
+      firstname: "Isabella",
+      lastname: "Moore",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 10,
+    name: {
+      firstname: "Liam",
+      lastname: "Clark",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 11,
+    name: {
+      firstname: "Ella",
+      lastname: "Wilson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 12,
+    name: {
+      firstname: "Ethan",
+      lastname: "Martin",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 13,
+    name: {
+      firstname: "Aria",
+      lastname: "Anderson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 14,
+    name: {
+      firstname: "Noah",
+      lastname: "White",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 15,
+    name: {
+      firstname: "Ava",
+      lastname: "Thompson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 16,
+    name: {
+      firstname: "James",
+      lastname: "Harris",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 17,
+    name: {
+      firstname: "Lily",
+      lastname: "Walker",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 18,
+    name: {
+      firstname: "Owen",
+      lastname: "Turner",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 19,
+    name: {
+      firstname: "Zoe",
+      lastname: "Scott",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 20,
+    name: {
+      firstname: "Mason",
+      lastname: "Jones",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 21,
+    name: {
+      firstname: "Aiden",
+      lastname: "Mitchell",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 22,
+    name: {
+      firstname: "Sofia",
+      lastname: "Williams",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 23,
+    name: {
+      firstname: "Elijah",
+      lastname: "Johnson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 24,
+    name: {
+      firstname: "Mila",
+      lastname: "Brown",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 25,
+    name: {
+      firstname: "Logan",
+      lastname: "Davis",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 26,
+    name: {
+      firstname: "Grace",
+      lastname: "Wilson",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 27,
+    name: {
+      firstname: "Carter",
+      lastname: "Miller",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 28,
+    name: {
+      firstname: "Evelyn",
+      lastname: "Taylor",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 29,
+    name: {
+      firstname: "Jackson",
+      lastname: "Moore",
+    },
+    course: "BSIT",
+  },
+  {
+    id: 30,
+    name: {
+      firstname: "Chloe",
+      lastname: "Clark",
+    },
+    course: "BSIT",
+  },
+  // ... (continue with more entries and "BSIT" for course)
+];
